020e22875dc61c7b9f3296bab009aec82019e683
Ram-Santosh <ram.santosh@techmojo.in>
2022-07-20 18:19:15 +0530

    css updated

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import Container from 'react-bootstrap/esm/Container';
 
 export default function Details () {
     var [card, selectedCard] = useState({});
@@ -34,14 +35,16 @@ export default function Details () {
     }
 
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={card.image} style={{objectFit:"cover", width: '18rem', height:"15vh"}}/>
-            <Card.Body>
-            <Card.Title>{card.title}</Card.Title>
-            <Card.Text>{card.description}</Card.Text>
-            <Button variant="primary" onClick={() => handleEdit(card.title,card.image,card.description)}>Edit</Button>&nbsp;&nbsp;
-            <Button variant="primary" onClick={() => handleDelete(card.title)}>Delete</Button>
-            </Card.Body>
-        </Card>
+        <Container className='mt-5'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={card.image} style={{objectFit:"cover", width: '18rem', height:"15vh"}}/>
+                <Card.Body>
+                <Card.Title>{card.title}</Card.Title>
+                <Card.Text>{card.description}</Card.Text>
+                <Button variant="primary" onClick={() => handleEdit(card.title,card.image,card.description)}>Edit</Button>&nbsp;&nbsp;
+                <Button variant="primary" onClick={() => handleDelete(card.title)}>Delete</Button>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
